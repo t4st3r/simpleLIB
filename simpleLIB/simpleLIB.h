1507c612802860832e4289b2bd66aee6e1e44652
Kryštof Foltyn <67323075+t4st3r@users.noreply.github.com>
2025-12-20 22:48:56 +0100

    New version upload

--- simpleLIB/simpleLIB.h
+++ simpleLIB/simpleLIB.h
@@ -6,41 +6,89 @@
 
 class TempPreset {
   public:
-  TempPreset();
+  TempPreset(int presNum);
   float temp = 0;
+  bool serialUsed;
+  int memoryAddress = 0;
+  int presetNumber = 0;
+  Stream &serial = Serial;
   bool inUse(int set);
-  void setAddress(int addr);
-  void initTemp(int _temp);
+  void initWith(int _temp, int addr, bool useSer = false, Stream &ser = Serial);
   void saveTo(EEPROM_SPI_WE& mem);
   void recallFrom(EEPROM_SPI_WE& mem);
-  int memoryAddress = 0;
+  
 };
 
 class BasicButton {
   public:
-  BasicButton();
+  BasicButton(int butNum);
   int state = false;
   int lastState = false;
+  int buttonNumber = 0;
+  bool wasPressed = false;
+  bool pressed = false;
+  bool serialUsed;
   PinName buttonPin;
+  Stream &serial = Serial;
   bool buttonPressed();
-  void initializeWithPin(PinName Bpin); //used instead of begin()
+  void initWith(PinName Bpin, bool useSer = false, Stream &ser = Serial); //used instead of begin()
+  void waitForPress();
+  bool beingPressed();
 
 };
 
 class TempSensor {
   public:
-  TempSensor();
+  TempSensor(int sensNum);
   float ntcB;
   float adcU;
-  float adcB;
+  float adcSteps;
   float rawTemp;
   float realTemp;
   float resAt25;
   float adcRealU;
+  int sensorNumber = 0;
+  bool serialUsed;
+  Stream &serial = Serial;
   PinName sensorPin;
-  void initWith(float adcRefV, float adcBitCount, float beta, PinName Tpin, float resAtRoomTemp);
+  void initWith(PinName sensPin, float adcRefV, float adcBits, float beta, float resAtRoomTemp, bool useSer = false, Stream &ser = Serial);
   float read();
 
-}
+};
+
+class CurrentSensor {
+  public:
+  CurrentSensor(int sensNum);
+  float rawCurrent;
+  float adcSteps;
+  float adcR;
+  float actualCurrent;
+  float sensorResistance;
+  float sensorVoltPerVolt;
+  int sensorNumber = 0;
+  bool serialUsed;
+  Stream &serial = Serial;
+  PinName currentPin;
+  void initWith(PinName currPin, float sensR, float sensVpV, float adcBits, float adcRange, bool useSer = false, Stream &ser = Serial);
+  float read();
+
+};
+
+class VoltageSensor {
+  public:
+  VoltageSensor(int sensNum);
+  float rawVoltage;
+  float adcSteps;
+  float adcR;
+  float actualVoltage;
+  float sensorVoltPerVolt;
+  int sensorNumber;
+  bool serialUsed;
+  Stream &serial = Serial;
+  PinName voltagePin;
+  void initWith(PinName voltPin, float sensVpV, float adcBits, float adcRange, bool useSer = false, Stream &ser = Serial);
+  float read();
+
+};
 
 #endif
