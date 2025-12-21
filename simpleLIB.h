4c1c55e18dae1a8cced62fa52e04fed418f3e23a
Kryštof Foltyn <67323075+t4st3r@users.noreply.github.com>
2025-12-21 23:24:37 +0100

    Modified class BasicButton for more flexibility

--- simpleLIB.h
+++ simpleLIB.h
@@ -21,19 +21,24 @@ class TempPreset {
 
 class BasicButton {
   public:
-  BasicButton(int butNum);
+  BasicButton(int butNum); //constructor, sets the number ID of button
   int state = false;
   int lastState = false;
   int buttonNumber = 0;
+  int mod;
+  int actOn;
   bool wasPressed = false;
   bool pressed = false;
   bool serialUsed;
   PinName buttonPin;
   Stream &serial = Serial;
-  bool buttonPressed();
-  void initWith(PinName Bpin, bool useSer = false, Stream &ser = Serial); //used instead of begin()
-  void waitForPress();
-  bool beingPressed();
+  bool buttonPressed(); //returns true if button press is detected, otherwise returns false
+  void initWith(PinName Bpin, int mode = 0, int activeOn = 0, bool useSer = false, Stream &ser = Serial); //used instead of begin()
+  /*Bpin: pin used by button; mode: 0 = internal pullup (button pulling down), 1 = internal pulldown (button pulling up), any other value = no internal pullup/down;
+  activeOn: 0 = input low active, 1 = input high active (only to be used with mode other than 0 or 1); useSer: true = serial monitoring used, false = serial monitoring not used, default false;
+  &ser = serial interface to be used for monitoring, default "Serial"*/
+  void waitForPress(); //waits until button is pressed
+  bool beingPressed(); //returns true if button is continuously pressed, otherwise false
 
 };
 
